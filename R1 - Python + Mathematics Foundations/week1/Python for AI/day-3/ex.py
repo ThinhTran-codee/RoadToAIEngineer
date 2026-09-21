bf0aa2d74e012e7a6409a793eb3f2c1dc1b8cc4f
Tran Viet Thinh <thinhkesat@gmail.com>
2026-09-21 16:44:50 +0700

    update loss decrease

--- R1 - Python + Mathematics Foundations/week1/Python for AI/day-3/ex.py
+++ R1 - Python + Mathematics Foundations/week1/Python for AI/day-3/ex.py
@@ -1,40 +1,27 @@
 import numpy as np
 
-#Traiining data
-
 X = np.array([1, 2, 3, 4, 5])
 y = np.array([3, 5, 7, 9, 11])
 
-#Parameters
-
-w=0.0
-b=0.0
+w = 0.0
+b = 0.0
 
 learning_rate = 0.01
-print("X:", X)
-print("X shape:", X.shape)
-print("y shape:", y.shape)
 
-for i in range (1000):
- print("iteration:", i)
-#Prediction
+for i in range(1000):
+    y_pred = X * w + b
 
- y_pred = X * w + b
+    loss = np.mean((y_pred - y) ** 2)
 
-#Loss
-loss  = np.mean((y_pred - y) ** 2)
+    dw = np.mean(2 * X * (y_pred - y))
+    db = np.mean(2 * (y_pred - y))
 
-#Gradient
-dw = np.mean(2 * X * (y_pred - y))
-db = np.mean(2 * (y_pred - y))
+    w = w - learning_rate * dw
+    b = b - learning_rate * db
 
-#Update
-w = w - learning_rate * dw
-b = b - learning_rate * db
+    if i % 100 == 0:
+        print(i, loss, w, b)
 
-if i % 100 == 0:
- print(i, loss, w, b)
-
-print("Final: ")
-print("w = ", w)
-print("b = ", b)
+print("Final:")
+print("w =", w)
+print("b =", b)
