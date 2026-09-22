44ead47122658ed1b9e0eec20a0bcee9223ed877
Tran Viet Thinh <thinhkesat@gmail.com>
2026-09-22 11:22:05 +0700

    save history loss

--- R1 - Python + Mathematics Foundations/week1/Python for AI/day-3/linear_regression.py
+++ R1 - Python + Mathematics Foundations/week1/Python for AI/day-3/linear_regression.py
@@ -4,14 +4,24 @@ def predict(X, w, b):
     return X * w + b
 
 def train(X, y, w, b, learning_rate, epochs):
+
+    loss_history = []
+
     for _ in range(epochs):
+
         y_pred = predict(X, w, b)
+
         loss = np.mean((y_pred - y) ** 2)
+
+        loss_history.append(loss)
+
         dw = np.mean(2 * X * (y_pred - y))
         db = np.mean(2 * (y_pred - y))
+
         w = w - learning_rate * dw
         b = b - learning_rate * db
-    return w, b
+
+    return w, b, loss_history
 
 X = np.array([1, 2, 3, 4, 5])
 y = np.array([3, 5, 7, 9, 11])
@@ -19,7 +29,18 @@ y = np.array([3, 5, 7, 9, 11])
 w = 0.0
 b = 0.0
 
-w, b = train(X, y, w, b, learning_rate=0.01, epochs=1000)
+w, b, loss_history = train(
+    X,
+    y,
+    w,
+    b,
+    learning_rate=0.01,
+    epochs=1000
+)
 
-print("w = ", w)
-print("b = ", b)
+print("w =", w)
+print("b =", b)
+
+print("First 5 losses:", loss_history[:5])
+print("Last 5 losses:", loss_history[-5:])
+print("Number of losses:", len(loss_history))
